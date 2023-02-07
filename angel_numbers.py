ab2908141d353ee55f32e15dc4ddda1aa3a1fd7b
Cameron Davis <socal5ever@gmail.com>
2023-02-06 21:32:11 -0800

    First Commit

---
+++ angel_numbers.py
@@ -0,0 +1,105 @@
+# Angel Number Project version 1.0
+# Reid Davis
+# February 6, 2023
+
+class Number:
+    def __init__(self):
+        self.meaning = ""
+        self.love = ""
+        self.twinflame = ""
+        self.financial = ""
+        self.career = ""
+        self.spiritual = ""
+        self.life = ""
+    
+    def __repr__(self):
+        return self.meaning
+
+one = Number()
+one.meaning = "In numerology, the number 1 reflects new beginnings, power, and action."
+one.love = ""
+one.twinflame = ""
+one.financial = ""
+one.career = """The number 1 serves as a reminder of your own personal power.\n
+                Seeing this angel number may be a push from your angels to manifest your goals and be proactive when it comes to the next phase of your life.\n
+                You have the power within you to create your own reality.\n
+                Seeing the angel number 1 is a sign of energy and power, so it may be time to work with the Law of Attraction and manifest your dreams!"""
+one.spiritual = """If you are seeing this number a lot, your guardian angels want you to experience new ideas regarding spirituality.\n
+                   The angel number 1 is often seen as a wake-up call in regard to spirituality.\n
+                   It asks you to put faith in the universe and embrace your spiritual side.\n
+                   Number 1 is a number of new beginnings, and this can be reflected in your approach to your spirituality"""
+one.life = ""
+
+two = Number()
+two.meaning = ""
+two.love = ""
+two.twinflame = ""
+two.financial = ""
+two.career = ""
+two.spiritual = ""
+two.life = ""
+
+three = Number()
+three.meaning = ""
+three.love = ""
+three.twinflame = ""
+three.financial = ""
+three.career = ""
+three.spiritual = ""
+three.life = ""
+
+four = Number()
+four.meaning = ""
+four.love = ""
+four.twinflame = ""
+four.financial = ""
+four.career = ""
+four.spiritual = ""
+four.life = ""
+
+five = Number()
+five.meaning = ""
+five.love = ""
+five.twinflame = ""
+five.financial = ""
+five.career = ""
+five.spiritual = ""
+five.life = ""
+
+six = Number()
+six.meaning = ""
+six.love = ""
+six.twinflame = ""
+six.financial = ""
+six.career = ""
+six.spiritual = ""
+six.life = ""
+
+seven = Number()
+seven.meaning = ""
+seven.love = ""
+seven.twinflame = ""
+seven.financial = ""
+seven.career = ""
+seven.spiritual = ""
+seven.life = ""
+
+eight = Number()
+eight.meaning = ""
+eight.love = ""
+eight.twinflame = ""
+eight.financial = ""
+eight.career = ""
+eight.spiritual = ""
+eight.life = ""
+
+nine = Number()
+nine.meaning = ""
+nine.love = ""
+nine.twinflame = ""
+nine.financial = ""
+nine.career = ""
+nine.spiritual = ""
+nine.life = ""
+    
+player = {'name': "", 'love': False, 'twinflame': False, 'financial': False, 'career': False, 'spiritual': False, 'life': False}
